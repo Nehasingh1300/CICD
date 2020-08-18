2aafd7ede4e1002ed1ad0839f1105cc770ca0035
1300nehasingh <1300nehasingh@gmail.com>
2020-08-18 14:44:58 +0530

    first commit

---
+++ cicdLab1.1/src/main/java/pkg1/test1.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Connecting Jenkins & Maven");
+	}
+
+}
